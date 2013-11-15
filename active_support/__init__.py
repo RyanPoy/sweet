78dfc4ab8059518d24cd20d4b85249dc4c799474
pengyi <ryanpoy@gmail.com>
2013-11-15 17:25:01 +0800

    - create a new package active_support - mv decorates.py, exceptions.py, inflection.py and support.py to active_support

---
+++ active_support/__init__.py
@@ -0,0 +1,5 @@
+#coding: utf8
+from pyrails.active_support.decorates import *
+from pyrails.active_support.exceptions import *
+from pyrails.active_support.inflection import *
+from pyrails.active_support.support import *
