2ca3a49abfa3b3a71ecca5eb57227531c0236a8d
pengyi <ryanpoy@gmail.com>
2013-11-19 22:51:03 +0800

    add more test for activerecord joins method

--- tests/test_activerecord_joins.py
+++ tests/test_activerecord_joins.py
@@ -20,17 +20,23 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from pyrails.active_record import ActiveRecord, belongs_to, has_one
+from pyrails.active_record import ActiveRecord, belongs_to, has_one, has_many
 from pyrails.tests import create_table, drop_table
 from pyrails.active_support import datetime2str
 import unittest
 
 
-class Father(ActiveRecord): 
-    has_one('pyrails.tests.test_association_belongs_to.Child')
+class Son(ActiveRecord):
+    belongs_to('pyrails.tests.test_activerecord_joins.Father', foreign_key='dad_id')
 
 class Child(ActiveRecord):
-    belongs_to(Father)
+    belongs_to('pyrails.tests.test_activerecord_joins.Father')
+
+class Father(ActiveRecord): 
+    has_one(Child)
+    has_many(Son, foreign_key='dad_id')
+
+
 
 
 class ActiveRecordJoinsTest(unittest.TestCase):
@@ -38,7 +44,8 @@ class ActiveRecordJoinsTest(unittest.TestCase):
     def setUp(self):
         drop_table('fathers')
         drop_table('children')
-        
+        drop_table('sons')
+
         create_table("""
 create table if not exists fathers (
     id int auto_increment,
@@ -54,11 +61,20 @@ create table if not exists children (
     PRIMARY KEY (id)
 );
 """)
+        create_table("""
+create table if not exists sons (
+    id int auto_increment,
+    name varchar(32) not null,
+    dad_id int,
+    PRIMARY KEY (id)
+);
+""")
 
     def tearDown(self):
         drop_table('fathers')
         drop_table('children')
-        
+        drop_table('sons')
+
     def test_belongs_to_query(self):
         fid = Father.create(name='Bob').id
         c = Child.create(created_at='2011-10-10 12:12:12', father_id=fid)
@@ -76,17 +92,8 @@ create table if not exists children (
         c = Child.joins('father').where('fathers.name = ?', 'pengyi').first
         self.assertEqual('2012-10-10 12:12:12', datetime2str(c.created_at))
 
-    # def test_cache_of_belongs_to_join(self):
-    #     f = Father.create(name='pengyi')
-    #     f.create_child(created_at='2012-10-10 12:12:12')
-
-    #     c = Child.first
-    #     self.assertFalse('father' in c.__dict__)
-
-    #     c = Child.joins('father').where('fathers.name = ?', 'pengyi').first
-    #     self.assertTrue('father' in c.__dict__)
-
     def test_has_one_join_query(self):
+
         f = Father.create(name='pengyi')
         f.create_child(created_at='2012-10-10 12:12:12')
 
@@ -96,6 +103,26 @@ create table if not exists children (
         f1 = Father.joins('child').where('children.created_at = ?', '2012-10-10 12:12:12').first
         self.assertEqual(f.id, f1.id)
 
+    def test_has_many_join_query(self):
+        f = Father.create(name='pengyi')
+        f.create_son(name="son1")
+
+        f1 = Father.joins('sons').where('sons.name = "son2"').first
+        self.assertIsNone(f1)
+        
+        f1 = Father.joins('sons').where('sons.name = "son1"').first
+        self.assertEqual(f.id, f1.id)
+
+
+    # def test_cache_of_belongs_to_join(self):
+    #     f = Father.create(name='pengyi')
+    #     f.create_child(created_at='2012-10-10 12:12:12')
+
+    #     c = Child.first
+    #     self.assertFalse('father' in c.__dict__)
+
+    #     c = Child.joins('father').where('fathers.name = ?', 'pengyi').first
+    #     self.assertTrue('father' in c.__dict__)
 
 if __name__ == '__main__':
     unittest.main()
