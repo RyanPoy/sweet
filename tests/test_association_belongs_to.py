a7e0d601cc22dfb35ca02c7e50b68a5f8b68ca58
pengyi <ryanpoy@gmail.com>
2013-11-20 21:00:39 +0800

    combine test_association_belongs_to_helper.py, test_association_has_and_belongs_to_many_helper.py, test_association_has_many_helper.py and test_association_has_one_helper.py to test_association_helper.py

--- tests/test_association_belongs_to.py
+++ tests/test_association_belongs_to.py
@@ -20,7 +20,7 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from pyrails.tests.test_association_belongs_to_helper import *
+from pyrails.tests.test_association_helper import *
 import unittest
 
 
--- tests/test_association_has_and_belongs_to_many.py
+++ tests/test_association_has_and_belongs_to_many.py
@@ -21,7 +21,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from pyrails.activerecord import ActiveRecord
-from pyrails.tests.test_association_has_and_belongs_to_many_helper import *
+from pyrails.tests.test_association_helper import *
 import unittest
 
 
