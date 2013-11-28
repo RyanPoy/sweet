ff46b68daf2b2b4c35189d9b5cdc09daf2f1beb3
pengyi <ryanpoy@gmail.com>
2013-11-28 10:22:39 +0800

    remove some code that does not used

--- __init__.py
+++ __init__.py
@@ -20,16 +20,4 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from pyrails.db import get_database
 
-
-def create_table(sql):
-    db = get_database(show_sql=False)
-    db.execute_rowcount(sql)
-    db.close()
-
-
-def drop_table(table_name):
-    db = get_database(show_sql=False)
-    db.execute_rowcount('drop table if exists %s' % table_name)
-    db.close()
