8a07434f0afc1f34859d673558be0a46653e50e7
pengyi <ryanpoy@gmail.com>
2016-03-07 18:17:06 +0800

    实现了has_one

---
+++ pyactive/relation/has_one.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*-
+from .relation import Relation
+from ..utils import *
+
+
+class HasOne(object):
+    
+    def __init__(self, target_class, owner_class=None, owner_attr=None, foreign_key=None):
+        """
+        target_class: 目标的 class
+        owner_class: 拥有者的class
+        foreign_key: 目标的 class 所在table的字段名
+        """
+        self._owner = owner_class
+        self._target = target_class
+        self._foreign_key = foreign_key
+        self._owner_attr = owner_attr
+        
+    @property
+    def owner(self):
+        return self._owner
+
+    @property
+    def target(self):
+        """_target is None, should be return None
+           _target is a active record class path, should be parse it and set a active record class to _target, then return it
+           _target is a active record class, should be return it   
+        """
+        if self._target is None:
+            return None
+        if isinstance(self._target, str):
+            self._target = import_object(self._target)
+        return self._target
+    
+    @property
+    def owner_attr(self):
+        if self._owner_attr:
+            return self._owner_attr
+        if self.owner is None:
+            return None
+        from ..record import ActiveRecord # lazy import
+        if not issubclass(self.owner, ActiveRecord):
+            return None
+        target = self.target
+        if target is None:
+            return None
+        self._owner_attr = singularize_of(target.table_name)
+        return self._owner_attr
+
+    @property
+    def target_pk_column(self):
+        target = self.target
+        return target.__pk__ if target else None
+
+    @property
+    def foreign_key(self):
+        """Phone belongs to User
+        owner_column must be user_id
+        """
+        if self._foreign_key:
+            return self._foreign_key
+        if self.owner is None:
+            return None
+        from ..record import ActiveRecord # lazy import
+        if not issubclass(self.owner, ActiveRecord):
+            return None
+        owner = self.owner
+        if owner is None:
+            return None
+        foreign_key = singularize_of(owner.table_name) + '_id'
+        if not self.target.has_column(foreign_key):
+            raise ColumnNotInColumns('"%s" not in %s columns' % (foreign_key, self.target.__name__))
+        self._foreign_key = foreign_key
+        return self._foreign_key
+
+    def __get__(self, instance, owner):
+        foreign_key_value = getattr(instance, instance.__pk__)
+        return self.target.where(**{self.foreign_key: foreign_key_value}).first()
+
+    def inject(self, owner=None):
+        if owner: self._owner = owner
+        self._owner.__relations__.append(self)
+        setattr(self._owner, self.owner_attr, self)
+    
+
+def has_one(target_class_or_classpath, foreign_key=None, owner_attr=""):
+    relation = HasOne(target_class=target_class_or_classpath, foreign_key=foreign_key, owner_attr=owner_attr)
+    Relation.push(relation)
+    return relation
---
+++ pyactive/tests/test_relation_has_one.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+from ..record import ActiveRecord
+from ..relation import has_one, HasOne
+from pyactive.utils import ColumnNotInColumns
+from datetime import datetime
+import unittest
+import fudge
+
+
+class Card(ActiveRecord):
+    __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+
+
+class RelationHasOneTestCase(unittest.TestCase):
+
+    def test_has_one_relation_init(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+    
+        class Phone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+
+        r = HasOne(target_class=Phone, owner_class=User, foreign_key='user_id', owner_attr='phone')
+        self.assertTrue(r.owner is User)
+        self.assertEqual('user_id', r.foreign_key)
+        self.assertTrue(r.target is Phone)
+        self.assertEqual('id', r.target_pk_column)
+        self.assertEqual('phone', r.owner_attr)
+
+    def test_has_one_relation_init_without_owner_attr(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+    
+        class CardPhone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+                
+        r = HasOne(target_class=CardPhone, owner_class=User, foreign_key='user_id')
+        self.assertTrue(r.owner is User)
+        self.assertEqual('user_id', r.foreign_key)
+        self.assertTrue(r.target is CardPhone)
+        self.assertEqual('id', r.target_pk_column)
+        self.assertEqual('card_phone', r.owner_attr)
+ 
+    def test_has_one_relation_init_without_foreign_key(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+   
+        class CardPhone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+        
+        r = HasOne(target_class=CardPhone, owner_class=User, owner_attr='card_phone')
+        self.assertTrue(r.owner is User)
+        self.assertEqual('user_id', r.foreign_key)
+        self.assertTrue(r.target is CardPhone)
+        self.assertEqual('id', r.target_pk_column)
+        self.assertEqual('card_phone', r.owner_attr)
+
+    def test_has_one_relation_init_should_raise_exception_if_relation_foreign_key_not_in_columns(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+  
+        class Phone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+  
+        r = HasOne(target_class=Phone, owner_class=User)
+        self.assertRaises(ColumnNotInColumns, lambda: r.foreign_key)
+ 
+    def test_has_one_relation_init_with_target_classpath(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+              
+        r = HasOne(target_class="pyactive.tests.test_relation_has_one.Card", owner_class=User)
+        self.assertTrue(r.owner is User)
+        self.assertEqual('user_id', r.foreign_key)
+        self.assertTrue(r.target is Card)
+        self.assertEqual('id', r.target_pk_column)
+        self.assertEqual('card', r.owner_attr)
+ 
+    @fudge.patch('pyactive.record.ar.Criteria')
+    def test_phone_belongs_to_user_relation(self, Criteria):
+        class Phone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+
+        class User(ActiveRecord):
+            __columns__ = ['id', 'name', 'created_at', 'updated_at']
+            has_one(Phone)
+  
+        Criteria.is_callable().returns_fake()\
+                .expects('from_').returns_fake()\
+                .expects('where').with_args(id=(10, )).returns_fake()\
+                .expects('first').returns(User(id=10, created_at=datetime.now(), updated_at=datetime.now(), name='py'))
+        u = User.find(10)
+        self.assertEqual('py', u.name)
+        Criteria.is_callable().returns_fake()\
+                .expects('from_').returns_fake()\
+                .expects('where').with_args(user_id=10).returns_fake()\
+                .expects('first').returns(Phone(id=1, created_at=datetime.now(), updated_at=datetime.now(), user_id=10))
+        p = u.phone
+        self.assertEqual(1, p.id)
+        self.assertTrue(isinstance(u.created_at, datetime))
+        self.assertTrue(isinstance(u.updated_at, datetime))
+        self.assertEqual(10, p.user_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
