f9a0ead9a1c8c8cdbb6c7338353728da5ac86d46
pengyi <ryanpoy@gmail.com>
2016-03-08 10:22:23 +0800

    实现了has_many

--- pyactive/tests/test_relation_has_many.py
+++ pyactive/tests/test_relation_has_many.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from ..record import ActiveRecord
-from ..relation import has_one, HasOne
+from ..relation import has_many, HasMany
 from pyactive.utils import ColumnNotInColumns
 from datetime import datetime
 import unittest
@@ -11,80 +11,80 @@ class Card(ActiveRecord):
     __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
 
 
-class RelationHasOneTestCase(unittest.TestCase):
+class RelationHasManyTestCase(unittest.TestCase):
 
-    def test_has_one_relation_init(self):
-        class User(ActiveRecord):
-            __columns__ = ['id', 'created_at', 'updated_at']
-    
-        class Phone(ActiveRecord):
-            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
-
-        r = HasOne(target_class=Phone, owner_class=User, foreign_key='user_id', owner_attr='phone')
-        self.assertTrue(r.owner is User)
-        self.assertEqual('user_id', r.foreign_key)
-        self.assertTrue(r.target is Phone)
-        self.assertEqual('id', r.target_pk_column)
-        self.assertEqual('phone', r.owner_attr)
-
-    def test_has_one_relation_init_without_owner_attr(self):
+    def test_has_many_relation_init(self):
         class User(ActiveRecord):
             __columns__ = ['id', 'created_at', 'updated_at']
     
         class CardPhone(ActiveRecord):
             __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
-                
-        r = HasOne(target_class=CardPhone, owner_class=User, foreign_key='user_id')
+
+        r = HasMany(target_class=CardPhone, owner_class=User, foreign_key='user_id', owner_attr='card_phones')
         self.assertTrue(r.owner is User)
         self.assertEqual('user_id', r.foreign_key)
         self.assertTrue(r.target is CardPhone)
         self.assertEqual('id', r.target_pk_column)
-        self.assertEqual('card_phone', r.owner_attr)
+        self.assertEqual('card_phones', r.owner_attr)
+
+    def test_has_many_relation_init_without_owner_attr(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+     
+        class CardPhone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+                 
+        r = HasMany(target_class=CardPhone, owner_class=User, foreign_key='user_id')
+        self.assertTrue(r.owner is User)
+        self.assertEqual('user_id', r.foreign_key)
+        self.assertTrue(r.target is CardPhone)
+        self.assertEqual('id', r.target_pk_column)
+        self.assertEqual('card_phones', r.owner_attr)
+  
+    def test_has_many_relation_init_without_foreign_key(self):
+        class User(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at']
+    
+        class CardPhone(ActiveRecord):
+            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
+         
+        r = HasMany(target_class=CardPhone, owner_class=User, owner_attr='card_phones')
+        self.assertTrue(r.owner is User)
+        self.assertEqual('user_id', r.foreign_key)
+        self.assertTrue(r.target is CardPhone)
+        self.assertEqual('id', r.target_pk_column)
+        self.assertEqual('card_phones', r.owner_attr)
  
-    def test_has_one_relation_init_without_foreign_key(self):
+    def test_has_many_relation_init_should_raise_exception_if_relation_foreign_key_not_in_columns(self):
         class User(ActiveRecord):
             __columns__ = ['id', 'created_at', 'updated_at']
    
-        class CardPhone(ActiveRecord):
-            __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
-        
-        r = HasOne(target_class=CardPhone, owner_class=User, owner_attr='card_phone')
-        self.assertTrue(r.owner is User)
-        self.assertEqual('user_id', r.foreign_key)
-        self.assertTrue(r.target is CardPhone)
-        self.assertEqual('id', r.target_pk_column)
-        self.assertEqual('card_phone', r.owner_attr)
-
-    def test_has_one_relation_init_should_raise_exception_if_relation_foreign_key_not_in_columns(self):
-        class User(ActiveRecord):
-            __columns__ = ['id', 'created_at', 'updated_at']
-  
         class Phone(ActiveRecord):
             __columns__ = ['id', 'created_at', 'updated_at']
-  
-        r = HasOne(target_class=Phone, owner_class=User)
+   
+        r = HasMany(target_class=Phone, owner_class=User)
         self.assertRaises(ColumnNotInColumns, lambda: r.foreign_key)
- 
-    def test_has_one_relation_init_with_target_classpath(self):
+  
+    def test_has_many_relation_init_with_target_classpath(self):
         class User(ActiveRecord):
             __columns__ = ['id', 'created_at', 'updated_at']
-              
-        r = HasOne(target_class="pyactive.tests.test_relation_has_one.Card", owner_class=User)
+               
+        r = HasMany(target_class="pyactive.tests.test_relation_has_many.Card", owner_class=User)
         self.assertTrue(r.owner is User)
         self.assertEqual('user_id', r.foreign_key)
         self.assertTrue(r.target is Card)
         self.assertEqual('id', r.target_pk_column)
-        self.assertEqual('card', r.owner_attr)
- 
+        self.assertEqual('cards', r.owner_attr)
+
     @fudge.patch('pyactive.record.ar.Criteria')
     def test_phone_belongs_to_user_relation(self, Criteria):
         class Phone(ActiveRecord):
             __columns__ = ['id', 'created_at', 'updated_at', 'user_id']
-
+ 
         class User(ActiveRecord):
             __columns__ = ['id', 'name', 'created_at', 'updated_at']
-            has_one(Phone)
-  
+            has_many(Phone)
+
         Criteria.is_callable().returns_fake()\
                 .expects('from_').returns_fake()\
                 .expects('where').with_args(id=(10, )).returns_fake()\
@@ -94,8 +94,14 @@ class RelationHasOneTestCase(unittest.TestCase):
         Criteria.is_callable().returns_fake()\
                 .expects('from_').returns_fake()\
                 .expects('where').with_args(user_id=10).returns_fake()\
-                .expects('first').returns(Phone(id=1, created_at=datetime.now(), updated_at=datetime.now(), user_id=10))
-        p = u.phone
+                .expects('first').returns([
+                    Phone(id=1, created_at=datetime.now(), updated_at=datetime.now(), user_id=10),
+                    Phone(id=2, created_at=datetime.now(), updated_at=datetime.now(), user_id=10),
+                    Phone(id=3, created_at=datetime.now(), updated_at=datetime.now(), user_id=10),
+                ])
+        ps = u.phones
+        self.assertEqual(3, len(ps))
+        p = ps[0]
         self.assertEqual(1, p.id)
         self.assertTrue(isinstance(p.created_at, datetime))
         self.assertTrue(isinstance(p.updated_at, datetime))
