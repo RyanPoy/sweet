b9dd5195174016f5bef2fc94a062c7c7c2e8dc79
pengyi <ryanpoy@gmail.com>
2018-07-05 18:40:01 +0800

    优化sql语句

--- query/sqlbuilder.py
+++ query/sqlbuilder.py
@@ -21,11 +21,13 @@ class SQLBuilder(object):
         self.tbname = tbname
         return self
 
-    def __add_quotation_marks_to_column_name(self, s):
-        return '"%s"' % s if s != '*' else '*'
+    def __add_quotation_marks(self, s):
+        if s == '*':
+            return s
+        return '%s%s%s' % (self.qutotation_marks, s, self.qutotation_marks)
 
     def to_sql(self):
-        return 'SELECT {columns} FROM "{tablename}"'.format(
-            columns = ', '.join(map(self.__add_quotation_marks_to_column_name, self._select)),
-            tablename = self.tbname
+        return 'SELECT {columns} FROM {tablename}'.format(
+            columns = ', '.join(map(self.__add_quotation_marks, self._select)),
+            tablename = self.__add_quotation_marks(self.tbname)
         )
--- tests/unit/test_mysql_sqlbuilder.py
+++ tests/unit/test_mysql_sqlbuilder.py
@@ -11,17 +11,17 @@ class MysqlSQLBuilderTest(TestCase):
     def test_basic_select(self):
         sb = self.get_builder()
         sb.select('*').from_('users')
-        self.assertEqual('SELECT * FROM "users"', sb.to_sql())
+        self.assertEqual('SELECT * FROM `users`', sb.to_sql())
 
     def test_multi_select(self):
         sb = self.get_builder()
         sb.select('id', 'name').from_('users')
-        self.assertEqual('SELECT "id", "name" FROM "users"', sb.to_sql())
+        self.assertEqual('SELECT `id`, `name` FROM `users`', sb.to_sql())
 
     def test_select_twice(self):
         sb = self.get_builder()
         sb.select('id').from_('users').select('name')
-        self.assertEqual('SELECT "id", "name" FROM "users"', sb.to_sql())
+        self.assertEqual('SELECT `id`, `name` FROM `users`', sb.to_sql())
 
 
 if __name__ == '__main__':
