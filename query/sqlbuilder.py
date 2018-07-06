aa4e025940358e94a6973d5d09270490a2e2bbe8
pengyi <ryanpoy@gmail.com>
2018-07-06 19:43:57 +0800

    重构

--- query/sqlbuilder.py
+++ query/sqlbuilder.py
@@ -2,7 +2,49 @@
 from sweet.utils import *
 
 
+class mydict(dict):
+
+    def __getattr__(self, k):
+        if k in self:
+            return self[k]
+        return super(__getattr__, k)
+
+
+class where_expr(object):
+
+    # logic, operator, 
+    ops = {
+        'gt': ('', '>'), 
+        'not_gt': ('not', '>'), 
+        'gte': ('', '>='), 
+        'not_gte': ('not', '>='), 
+        'lt': ('', '<'), 
+        'not_lt': ('not', '<'), 
+        'lte': ('', '<='), 
+        'not_lte': ('not', '<='),
+        'not': ('not', '!=')
+    }
+
+    def __init__(self, s):
+        vs = s.split('__')
+        if len(vs) == 1: # 没有找到 "__"
+            self.logic, self.column, self.operator = '', s, '='
+        elif vs[-1] in self.ops: # 后缀在 ops里面
+            self.logic, self.operator = self.ops[vs[-1]]
+            self.column = s[:-len(vs[-1])-2]
+        else:
+            self.logic, self.column, self.operator = '', s, '='
+
+
+
 class SQLBuilder(object):
+
+    operators = {
+        'gt': '>',
+        'gte': '>=',
+        'lt': '<',
+        'lte': '<=',
+    }
 
     def __init__(self):
         self.tbname = ''
@@ -11,6 +53,7 @@ class SQLBuilder(object):
         self._where = []
         self._or = []
         self._where_bindings = []
+        self._bindings = []
 
     def distinct(self):
         self._distinct = True
@@ -23,40 +66,51 @@ class SQLBuilder(object):
 
     @property
     def bindings(self):
-        return self._where_bindings
+        return self._bindings
 
     def from_(self, tbname):
         self.tbname = tbname
         return self
 
     def where(self, **kwargs):
-        return self.__where_or_where_not(False, True, **kwargs)
-
-    def where_not(self, **kwargs):
-        return self.__where_or_where_not(True, True, **kwargs)
-
-    def or_(self, **kwargs):
-        return self.__where_or_where_not(False, False, **kwargs)
-
-    def or_not(self, **kwargs):
-        return self.__where_or_where_not(True, False, **kwargs)
-
-    def __where_or_where_not(self, _not, _and, **kwargs):
-        _in, _is, eq = 'IN', 'IS', '='
-        if _not:
-            _in, _is, eq = 'NOT IN', 'IS NOT', '!='
-        where_list = self._where if _and else self._or
-
         for k, v in kwargs.items():
-            if is_array(v):
-                where_list.append('%s %s (%s)' % (self.__aqm(k), _in, ', '.join(['%s']*len(v))))
-                self._where_bindings.extend(v)
-            elif v is None:
-                where_list.append('%s %s NULL' % (self.__aqm(k), _is) )
-            else:
-                where_list.append('%s %s %%s' % (self.__aqm(k), eq) )
-                self._where_bindings.append(v)
+            expr = where_expr(k)
+            self._where.append(mydict(
+                logic = expr.logic,
+                column = expr.column,
+                operator = expr.operator
+            ))
+            self._where_bindings.append(v)
         return self
+        # return self.__where(False, True, **kwargs)
+
+    # def where_not(self, **kwargs):
+    #     return self.__where(True, True, **kwargs)
+
+    # def or_(self, **kwargs):
+    #     return self.__where(False, False, **kwargs)
+
+    # def or_not(self, **kwargs):
+    #     return self.__where(True, False, **kwargs)
+
+    # def __where(self, logic, **kwargs):
+    #     pass
+        # _in, _is, eq = 'IN', 'IS', '='
+        # if _not:
+        #     _in, _is, eq = 'NOT IN', 'IS NOT', '!='
+        # where_list = self._where if _and else self._or
+
+        # for k, v in kwargs.items():
+        #     if is_array(v):
+        #         where_sqls.append('%s %s (%s)' % (self.__aqm(k), _in, ', '.join(['%s']*len(v))))
+        #         self._where_bindings.extend(v)
+        #     elif v is None:
+        #         where_sqls.append('%s %s NULL' % (self.__aqm(k), _is) )
+        #     else:
+        #         where_sqls.append('%s %s %%s' % (self.__aqm(k), eq) )
+        #         self._where_bindings.append(v)
+
+        # return self
 
     def __add_quotation_marks(self, s):
         if s == '*':
@@ -70,14 +124,24 @@ class SQLBuilder(object):
             columns = ', '.join(map(self.__aqm, self._select)) if self._select else '*',
             tablename = self.__aqm(self.tbname)
         )
-        has_where = False
+        where_sqls = []
         if self._where:
-            has_where = True
-            sql = '%s WHERE %s' % (sql, ' AND '.join(self._where))
-        if self._or:
-            if not has_where:
-                sql = '%s WHERE %s' % (sql, ' OR '.join(self._or))
-            else:
-                sql = '%s %s' % (sql, ' OR '.join(self._or))
+            for i, w in enumerate(self._where):
+                k, op = w.column, w.operator
+                v = self._where_bindings[i]
+                if is_array(v):
+                    where_sqls.append('%s IN (%s)' % (self.__aqm(k), ', '.join(['%s']*len(v))))
+                    self._bindings.extend(v)
+                elif v is None:
+                    where_sqls.append('%s IS NULL' % self.__aqm(k) )
+                else:
+                    where_sqls.append('%s %s %%s' % (self.__aqm(k), op) )
+                    self._bindings.append(v)
+            sql = '%s WHERE %s' % (sql, ' AND '.join(where_sqls))
+        # if self._or:
+        #     if not has_where:
+        #         sql = '%s WHERE %s' % (sql, ' OR '.join(self._or))
+        #     else:
+        #         sql = '%s %s' % (sql, ' OR '.join(self._or))
  
         return sql
--- tests/unit/test_mysql_sqlbuilder.py
+++ tests/unit/test_mysql_sqlbuilder.py
@@ -46,59 +46,83 @@ class MysqlSQLBuilderTest(TestCase):
         self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `name` IS NULL', sb.sql)
         self.assertEqual([1, 2, 3], sb.bindings)
 
+    def test_where_gt(self):
+        sb = self.get_builder()
+        sb.select('*').from_('users').where(id=[1, 2, 3], age__gt=30)
+        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `age` > %s', sb.sql)
+        self.assertEqual([1, 2, 3, 30], sb.bindings)
+
+    def test_where_gte(self):
+        sb = self.get_builder()
+        sb.select('*').from_('users').where(id=[1, 2, 3], age__gte=30)
+        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `age` >= %s', sb.sql)
+        self.assertEqual([1, 2, 3, 30], sb.bindings)
+
+    def test_where_lt(self):
+        sb = self.get_builder()
+        sb.select('*').from_('users').where(id=[1, 2, 3], age__lt=30)
+        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `age` < %s', sb.sql)
+        self.assertEqual([1, 2, 3, 30], sb.bindings)
+
+    def test_where_lte(self):
+        sb = self.get_builder()
+        sb.select('*').from_('users').where(id=[1, 2, 3], age__lte=30)
+        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `age` <= %s', sb.sql)
+        self.assertEqual([1, 2, 3, 30], sb.bindings)
+
     def test_where_not(self):
         sb = self.get_builder()
-        sb.select('*').from_('users').where_not(id=1, name='ryanpoy')
+        sb.select('*').from_('users').where(id__not=1, name__not='ryanpoy')
         self.assertEqual('SELECT * FROM `users` WHERE `id` != %s AND `name` != %s', sb.sql)
         self.assertEqual([1, 'ryanpoy'], sb.bindings)
 
-    def test_where_not_null(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').where(id=[1, 2, 3]).where_not(name=None)
-        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `name` IS NOT NULL', sb.sql)
-        self.assertEqual([1, 2, 3], sb.bindings)
+    # def test_where_not_null(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').where(id=[1, 2, 3]).where_not(name=None)
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) AND `name` IS NOT NULL', sb.sql)
+    #     self.assertEqual([1, 2, 3], sb.bindings)
 
-    def test_where_not_in(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').where_not(id=[1, 2, 3], name='ryanpoy')
-        self.assertEqual('SELECT * FROM `users` WHERE `id` NOT IN (%s, %s, %s) AND `name` != %s', sb.sql)
-        self.assertEqual([1, 2, 3, 'ryanpoy'], sb.bindings)
+    # def test_where_not_in(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').where_not(id=[1, 2, 3], name='ryanpoy')
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` NOT IN (%s, %s, %s) AND `name` != %s', sb.sql)
+    #     self.assertEqual([1, 2, 3, 'ryanpoy'], sb.bindings)
 
-    def test_or_(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').or_(id=1, name='ryanpoy')
-        self.assertEqual('SELECT * FROM `users` WHERE `id` = %s OR `name` = %s', sb.sql)
-        self.assertEqual([1, 'ryanpoy'], sb.bindings)
+    # def test_or_(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').or_(id=1, name='ryanpoy')
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` = %s OR `name` = %s', sb.sql)
+    #     self.assertEqual([1, 'ryanpoy'], sb.bindings)
 
-    def test_or_in(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').or_(id=[1, 2, 3], name='ryanpoy')
-        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) OR `name` = %s', sb.sql)
-        self.assertEqual([1, 2, 3, 'ryanpoy'], sb.bindings)
+    # def test_or_in(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').or_(id=[1, 2, 3], name='ryanpoy')
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) OR `name` = %s', sb.sql)
+    #     self.assertEqual([1, 2, 3, 'ryanpoy'], sb.bindings)
 
-    def test_or_null(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').or_(id=[1, 2, 3], name=None)
-        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) OR `name` IS NULL', sb.sql)
-        self.assertEqual([1, 2, 3], sb.bindings)
+    # def test_or_null(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').or_(id=[1, 2, 3], name=None)
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) OR `name` IS NULL', sb.sql)
+    #     self.assertEqual([1, 2, 3], sb.bindings)
 
-    def test_or_not(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').or_not(id=1, name='ryanpoy')
-        self.assertEqual('SELECT * FROM `users` WHERE `id` != %s OR `name` != %s', sb.sql)
-        self.assertEqual([1, 'ryanpoy'], sb.bindings)
+    # def test_or_not(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').or_not(id=1, name='ryanpoy')
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` != %s OR `name` != %s', sb.sql)
+    #     self.assertEqual([1, 'ryanpoy'], sb.bindings)
 
-    def test_or_not_null(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').or_(id=[1, 2, 3]).or_not(name=None)
-        self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) OR `name` IS NOT NULL', sb.sql)
-        self.assertEqual([1, 2, 3], sb.bindings)
+    # def test_or_not_null(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').or_(id=[1, 2, 3]).or_not(name=None)
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` IN (%s, %s, %s) OR `name` IS NOT NULL', sb.sql)
+    #     self.assertEqual([1, 2, 3], sb.bindings)
 
-    def test_or_not_in(self):
-        sb = self.get_builder()
-        sb.select('*').from_('users').or_not(id=[1, 2, 3], name='ryanpoy')
-        self.assertEqual('SELECT * FROM `users` WHERE `id` NOT IN (%s, %s, %s) OR `name` != %s', sb.sql)
-        self.assertEqual([1, 2, 3, 'ryanpoy'], sb.bindings)
+    # def test_or_not_in(self):
+    #     sb = self.get_builder()
+    #     sb.select('*').from_('users').or_not(id=[1, 2, 3], name='ryanpoy')
+    #     self.assertEqual('SELECT * FROM `users` WHERE `id` NOT IN (%s, %s, %s) OR `name` != %s', sb.sql)
+    #     self.assertEqual([1, 2, 3, 'ryanpoy'], sb.bindings)
 
 if __name__ == '__main__':
     import unittest
