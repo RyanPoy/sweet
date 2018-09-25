9abacaca94cf606e90c75b12493e1ffac508a3d1
pengyi <ryanpoy@gmail.com>
2018-09-25 19:10:33 +0800

    pass all tests

--- tests/unit/database/test_mysql_table_update.py
+++ tests/unit/database/test_mysql_table_update.py
@@ -20,21 +20,21 @@ class MySQLTableUpdateTest(TestCase):
 
     def test_increase(self):
         def _(sql, *params):
-            self.assertEqual('UPDATE `users` SET `age` = `age` + %s WHERE `age` >= %s OR `name` = %s', sql)
-            self.assertEqual([10, 40, 'ryanpoy'], list(params))
+            self.assertEqual('UPDATE `users` SET `age` = `age` + %s, `score` = `score` + %s WHERE `age` >= %s OR `name` = %s', sql)
+            self.assertEqual([10, 20, 40, 'ryanpoy'], list(params))
             return 3
         tb = self.get_table().where(age__gte=40).or_(name="ryanpoy")
         tb.db.execute_rowcount = _
-        self.assertEqual(3, tb.increase(age=10))
+        self.assertEqual(3, tb.increase(age=10, score=20))
 
     def test_decrease(self):
         def _(sql, *params):
-            self.assertEqual('UPDATE `users` SET `age` = `age` - %s WHERE `age` >= %s OR `name` = %s', sql)
-            self.assertEqual([10, 40, 'ryanpoy'], list(params))
+            self.assertEqual('UPDATE `users` SET `age` = `age` - %s, `score` = `score` - %s WHERE `age` >= %s OR `name` = %s', sql)
+            self.assertEqual([10, 20, 40, 'ryanpoy'], list(params))
             return 3
         tb = self.get_table().where(age__gte=40).or_(name="ryanpoy")
         tb.db.execute_rowcount = _
-        self.assertEqual(3, tb.decrease(age=10))
+        self.assertEqual(3, tb.decrease(age=10, score=20))
 
 
 if __name__ == '__main__':
