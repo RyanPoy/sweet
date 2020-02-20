31fb6bd7f6533383e9670378ddb885885a2778a5
pengyi <ryanpoy@gmail.com>
2020-02-20 12:57:16 +0800

    improve complex tests

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -12,7 +12,7 @@ class ComplexTest(TestCase):
             UserForTest("Lily", 10)
         ]
  
-    def test_complex(self):
+    def test_complex1(self):
         loader = MemLoader({
             "base.html": 
 """
@@ -54,6 +54,89 @@ class ComplexTest(TestCase):
 </html>
 """.split()), ''.join(r.split()))
     
+    def test_complex2(self):
+        loader = MemLoader({
+            "base.html": 
+"""
+<html>
+    <header>
+        <title><%block title%><% end %></title>
+        <body><%block body%><% end %></body>
+    </header>
+</html>
+""",
+            "minibase.html": 
+"""
+<% extends base.html %>
+<% block title %>首页<% end %>
+""",
+            "index.html": 
+"""
+<% extends minibase.html %>
+<% block body %>主体<% end %>
+""",
+        })
+        r = loader.load('index.html').render(users=self.users)
+        self.assertEqual(''.join("""
+<html>
+  <header>
+    <title>首页</title>
+    <body>主体</body>
+  </header>
+</html>
+""".split()), ''.join(r.split()))
+        
+    def test_complex3(self):
+        loader = MemLoader({
+            "base.html": 
+"""
+<html>
+    <header>
+        <title><%block title%><% end %></title>
+        <body><%block body%><% end %></body>
+    </header>
+</html>
+""",
+            "minibase.html": 
+"""
+<% extends base.html %>
+<% block title %>首页<% end %>
+""",
+            "index.html": 
+"""
+<% extends minibase.html %>
+<% block title %>首页<% end %>
+<% block body %><% include user_body.html %><% end %>
+""",
+            "user_body_base.html": """
+<ul>
+    <% block users %> <% end %>
+</ul>
+""",
+            "user_body.html": """
+<% extends user_body_base.html %>
+<% block users %>
+  <% for u in users%>
+      <% include user.html %>
+  <% end %>
+<% end %>
+""",
+            "user.html": """<li><%= u.name %>|<%= u.age %></li>"""
+        })
+        r = loader.load('index.html').render(users=self.users)
+        self.assertEqual(''.join("""
+<html>
+  <header>
+    <title>首页</title>
+    <body>
+      <ul>
+        <li>露西|20</li>
+        <li>Lily|10</li>
+      </ul>
+    </body>
+  </header>
+</html>
+""".split()), ''.join(r.split()))
 
 if __name__ == '__main__':
     unittest.main()
