35725129666c33750ed4822bcb5a27bbd67b6a65
pengyi <ryanpoy@gmail.com>
2020-05-18 17:57:16 +0800

    impl complex include: has_one and has_many same time

--- tests/integration/test_relation_include_mysql.py
+++ tests/integration/test_relation_include_mysql.py
@@ -311,6 +311,25 @@ class TestRelationIncludeMysql(TestCase):
             self.assertEqual('SELECT * FROM `articles_tags` WHERE `tag_id` IN (%s, %s, %s)', FakeDB.SQLS[1])
             self.assertEqual('SELECT * FROM `articles` WHERE `id` IN (%s, %s, %s, %s, %s, %s, %s, %s)', FakeDB.SQLS[2])
 
+    def test_include_has_one_and_has_many(self):
+        user1 = User.create(name="Jon", age=31)
+        user2 = User.create(name="Lily", age=20)
+        Mobile.create(name="Nokia", user=user1)
+        Mobile.create(name="IPhone", user=user1)
+        Mobile.create(name="Vivo", user=user2)
+        Mobile.create(name="Xiaomi", user=user2)
+        Car.create(name="Benz", user=user1)
+        Car.create(name="Mazda", user=user2)
+
+        with self.mock_db():
+            for u in User.include("mobiles", "car").all():
+                u.car.name
+                for m in u.mobiles.all():
+                    m.name
+            self.assertEqual(3, len(FakeDB.SQLS))
+            self.assertEqual('SELECT * FROM `users`', FakeDB.SQLS[0])
+            self.assertEqual('SELECT * FROM `mobiles` WHERE `user_id` IN (%s, %s)', FakeDB.SQLS[1])
+            self.assertEqual('SELECT * FROM `cars` WHERE `user_id` IN (%s, %s)', FakeDB.SQLS[2])
 
 if __name__ == '__main__':
     import unittest
