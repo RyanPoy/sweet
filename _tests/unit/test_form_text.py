d5af213fda78c308e4531d277a250e63f99e8ca7
pengyi <ryanpoy@gmail.com>
2020-05-20 11:18:34 +0800

    form text for model

--- _tests/unit/test_form_text.py
+++ _tests/unit/test_form_text.py
@@ -1,5 +1,5 @@
 # coding: utf8
-from sweet._tests import TestCase
+from sweet._tests import TestCase, User
 from sweet.template import Template
 
 
@@ -95,6 +95,19 @@ class TestFormText(TestCase):
 </form>
 """, t.render())
         
+    def test_for_model(self):
+
+        t = Template("""
+<%= using form(action="/user/new", model=user) do f %>
+    <%= f.text('name') %>
+<% end %>
+""")
+        self.assertEqual("""
+<form action="/user/new" method="GET" accept-charset="UTF8">
+    <input id="user_name" name="user['name']" type="text" value="Jon" />
+</form>
+""", t.render(user=User(name="Jon")))
+
 
 if __name__ == '__main__':
     import unittest
