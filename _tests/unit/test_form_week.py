60e7104fb86fe7fed6dbef887f73e54f10ff0c87
pengyi <ryanpoy@gmail.com>
2020-05-20 11:31:14 +0800

    form week for model

--- _tests/unit/test_form_week.py
+++ _tests/unit/test_form_week.py
@@ -1,5 +1,5 @@
 # coding: utf8
-from sweet._tests import TestCase
+from sweet._tests import TestCase, User
 from sweet.template import Template
 
 
@@ -40,6 +40,18 @@ class TestFormWeek(TestCase):
 </form>
 """, t.render())
         
+    def test_for_model(self):
+
+        t = Template("""
+<%= using form(action="/user/new", model=user) do f %>
+    <%= f.week("born_week") %>
+<% end %>
+""")
+        self.assertEqual("""
+<form action="/user/new" method="GET" accept-charset="UTF8">
+    <input id="user_born_week" name="user['born_week']" type="week" value="34" />
+</form>
+""", t.render(user=User(born_week=34)))
 
 if __name__ == '__main__':
     import unittest
