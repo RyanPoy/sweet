b09741aee84035713e010f76b60e52f2b0a4a4ae
pengyi <ryanpoy@gmail.com>
2020-05-21 10:18:42 +0800

    use mock

--- _tests/unit/test_recordset_insert_for_mysql.py
+++ _tests/unit/test_recordset_insert_for_mysql.py
@@ -1,4 +1,5 @@
 #coding: utf8
+from unittest import mock
 from sweet._tests import TestCase
 from sweet.database.recordset import MySQLRecordset
 
@@ -10,46 +11,39 @@ class TestRecordsetInsertForMySQL(TestCase):
         return MySQLRecordset(db=FakeDB(), tbname="users")
 
     def test_insert_an_record(self):
-        def _(sql, *params):
-            self.assertEqual('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s)', sql)
-            self.assertEqual([3, "Poy", 33], list(params))
-            return 1
-        tb = self.get_recordset()
-        tb.db.execute_rowcount = _
-        self.assertEqual(1, tb.insert(id=3, name="Poy", age=33))
+        db = mock.MagicMock('db')
+        db.execute_rowcount = mock.MagicMock(return_value=1)
+
+        tb = MySQLRecordset(db=db, tbname="users")
+        tb.insert(id=3, name="Poy", age=33)
+        db.execute_rowcount.assert_called_once_with('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s)', *[3, "Poy", 33])
 
     def test_insert_getid(self):
-        def _(sql, *params):
-            self.assertEqual('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s)', sql)
-            self.assertEqual([3, "Poy", 33], list(params))
-            return 1
-        tb = self.get_recordset()
-        tb.db.execute_lastrowid = _
-        self.assertEqual(1, tb.insert_getid(id=3, name="Poy", age=33))
+        db = mock.MagicMock('db')
+        db.execute_lastrowid = mock.MagicMock(return_value=1)
+        
+        tb = MySQLRecordset(db=db, tbname="users")
+        tb.insert_getid(id=3, name="Poy", age=33)
+        db.execute_lastrowid.assert_called_once_with('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s)', *[3, "Poy", 33])
 
     def test_insert_an_record_with_a_dict(self):
-        def _(sql, *params):
-            self.assertEqual('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s)', sql)
-            self.assertEqual([3, "Poy", 33], list(params))
-            return 1
-        tb = self.get_recordset()
-        tb.db.execute_rowcount = _
-        self.assertEqual(1, tb.insert(dict(id=3, name="Poy", age=33)))
+        db = mock.MagicMock('db')
+        db.execute_rowcount = mock.MagicMock(return_value=1)
+        
+        tb = MySQLRecordset(db=db, tbname="users")
+        tb.insert(dict(id=3, name="Poy", age=33))
+        db.execute_rowcount.assert_called_once_with('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s)', *[3, "Poy", 33])
 
     def test_insert_multple_records(self):
-        def _(sql, *params):
-            self.assertEqual('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s), (%s, %s, %s)', sql)
-            self.assertEqual([3, "Poy", 33, 4, "Ryan", 44], list(params))
-            return 2
-        tb = self.get_recordset()
-        tb.db.execute_rowcount = _
-        self.assertEqual(
-            2, 
-            tb.insert([
-                dict(id=3, name="Poy", age=33),
-                dict(id=4, name="Ryan", age=44),
-            ])
-        )
+        db = mock.MagicMock('db')
+        db.execute_rowcount = mock.MagicMock(return_value=2)
+        
+        tb = MySQLRecordset(db=db, tbname="users")
+        tb.insert([
+            dict(id=3, name="Poy", age=33),
+            dict(id=4, name="Ryan", age=44),
+        ])
+        db.execute_rowcount.assert_called_once_with('INSERT INTO `users` (`id`, `name`, `age`) VALUES (%s, %s, %s), (%s, %s, %s)', *[3, "Poy", 33, 4, "Ryan", 44])
 
 
 if __name__ == '__main__':
