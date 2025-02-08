01aae437c82f72e04cbbb4d3dcf6e1bf462869fa
pengyi <ryanpoy@gmail.com>
2025-02-08 08:41:11 +0800

    fix bug

--- sweet/sequel/terms/values.py
+++ sweet/sequel/terms/values.py
@@ -9,25 +9,28 @@ if TYPE_CHECKING:
     from sweet.sequel.terms.name import Name
 
 # 基本数据类型，在sql中，有 str, int, float, Decimal, bool, date, datetime, list, tuple, None, bytes
-
 Basic = Union[int, float, str, Decimal, bool, date, datetime, list, tuple, None, bytes]
 
 # Value 用在 set column = Value
 Value1 = Union[Basic, 'Fn', 'Name']
 
 RawType = Union[int, float, str, Decimal, bool, date, datetime, list, tuple, None, bytes]
-ValueType = [RawType, 'Fn', 'Name']
+ValueType = Union[RawType, 'Fn', 'Name']
+
+
+# // @todo: Binary 里面应该使用 ValueType 和 Value
+# // @todo: Q 里面应该使用 ValueType 和 Value
 
 
 @dataclass
 class Value:
-    v: Basic | 'Fn' | 'Name'
+    v : ValueType
 
 
 class Values:
 
     def __init__(self, *args: ValueType) -> None:
-        self.vs: Tuple[Value, ...] = args
+        self.vs: Tuple[Value, ...] = tuple(Value(x) for x in args)
 
     def is_empty(self):
         return self.vs is None or len(self.vs) == 0
@@ -40,41 +43,15 @@ class Values:
 
 
 class ValuesList:
-    """
-    Represents a collection of rows to be used in SQL statements, such as INSERT or UPDATE.
-    Each row is a tuple of value, and all rows must have consistent column length.
-
-    Usage:
-        # initialize with a single row
-        vs = ValuesList(1, "lily", 20)
-
-        # add a single row
-        vs.append([(2, "lucy", 32)])
-
-        # add multiple rows
-        vs.append([ (3, "jimy", 15), (4, "abc", 8) ])
-    """
 
     def __init__(self, *args: Values) -> None:
         self.data: [Values] = []
         self.append(*args)
 
     def is_empty(self):
-        """
-        Check if the ValuesList contains any rows.
-
-        :return: True if the ValuesList is empty, otherwise False
-        """
         return False if self.data else True
 
     def append(self, *args: Values) -> Self:
-        """
-        Append the row to the ValuesList. All rows must have consistent column length as existing rows.
-
-        :param values: A list of tuples where each tuple represents a row to be added.
-        :raises ValueError: If the rows have inconsistent length.
-        :return: The current instance of ValuesList.
-        """
         args = [x for x in args if not x.is_empty()]
         self.check_values(*args)
         self.data.extend(args)
