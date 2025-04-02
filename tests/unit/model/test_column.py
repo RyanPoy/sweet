b0e7283ac988c8c306c9e409dcdde4665e1c3743
pengyi <ryanpoy@gmail.com>
2025-04-02 15:11:41 +0800

    add pytest

--- tests/unit/model/test_column.py
+++ tests/unit/model/test_column.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from datetime import date, datetime, time
 from decimal import Decimal
 
@@ -7,101 +7,89 @@ from sweet.model.columns import BinaryColumn, BooleanColumn, CharColumn, DateCol
 from sweet.model import Model
 
 
-class TestColumn(unittest.TestCase):
+def test_unavailable_length():
+    with pytest.raises(ValueError, match="The length must be greater than zero, but got -10"):
+        CharColumn(length=-10)
 
-    def test_unavailable_length(self):
-        with self.assertRaises(ValueError) as ex:
-            CharColumn(length=-10)
-        self.assertEqual("The length must be greater than zero, but got -10", str(ex.exception))
+    with pytest.raises(ValueError, match='The length must be greater than zero, but got 0'):
+        TextColumn(length=0)
 
-        with self.assertRaises(ValueError) as ex:
-            TextColumn(length=0)
-        self.assertEqual('The length must be greater than zero, but got 0', str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            BinaryColumn(length=-10)
-
-    def test_name_is_not_none_when_defined_in_model(self):
-        class Demo(Model):
-            id = IntColumn()
-            name = CharColumn("demo_name")
-
-        self.assertEqual('demo_name', Demo.table.columns.name.name)
-        self.assertEqual('id', Demo.table.columns.id.name)
-
-    def test_value(self):
-        col = CharColumn()
-        col.value = "abc"
-        self.assertEqual("abc", col.value)
-
-        col = TextColumn()
-        col.value = "abc"
-        self.assertEqual("abc", col.value)
-
-        col = BinaryColumn()
-        col.value = "abc"
-        self.assertEqual(b"abc", col.value)
-
-        col = IntColumn()
-        col.value = 10
-        self.assertEqual(10, col.value)
-
-        col = BooleanColumn()
-        col.value = "y"
-        self.assertTrue(col.value)
-
-        col.value = "n"
-        self.assertFalse(col.value)
-
-        col = FloatColumn()
-        col.value = " 12.33"
-        self.assertEqual(12.33, col.value)
-
-        col = DecimalColumn()
-        col.value = "12.45"
-        self.assertEqual(Decimal("12.45"), col.value)
-
-        col = DateColumn()
-        col.value = "2016-10-9"
-        self.assertEqual(date(2016, 10, 9), col.value)
-
-        col = DatetimeColumn()
-        col.value = "2016-10-9 11:12:3"
-        self.assertEqual(datetime(2016, 10, 9, 11, 12, 3), col.value)
-
-        col = TimeColumn()
-        col.value = "11:12:3"
-        self.assertEqual(time(11, 12, 3), col.value)
-
-    def test_value_error(self):
-        with self.assertRaises(ValueError) as ex:
-            BinaryColumn().value = 19
-        self.assertEqual("Can't purify 19, it's a int type.", str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            IntColumn().value = "abc"
-        self.assertEqual("Can't purify abc, it's a str type.", str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            FloatColumn().value = "abc"
-        self.assertEqual("Can't purify abc, it's a str type.", str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            DecimalColumn().value = "abc"
-        self.assertEqual("Can't purify abc, it's a str type.", str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            DateColumn().value = "abc"
-        self.assertEqual("Can't purify abc, it's a str type.", str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            DatetimeColumn().value = "abc"
-        self.assertEqual("Can't purify abc, it's a str type.", str(ex.exception))
-
-        with self.assertRaises(ValueError) as ex:
-            TimeColumn().value = "abc"
-        self.assertEqual("Can't purify abc, it's a str type.", str(ex.exception))
+    with pytest.raises(ValueError):
+        BinaryColumn(length=-10)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_name_is_not_none_when_defined_in_model():
+    class Demo(Model):
+        id = IntColumn()
+        name = CharColumn("demo_name")
+
+    assert 'demo_name' == Demo.table.columns.name.name
+    assert 'id' == Demo.table.columns.id.name
+
+
+def test_value():
+    col = CharColumn()
+    col.value = "abc"
+    assert "abc" == col.value
+
+    col = TextColumn()
+    col.value = "abc"
+    assert "abc" == col.value
+
+    col = BinaryColumn()
+    col.value = "abc"
+    assert b"abc" == col.value
+
+    col = IntColumn()
+    col.value = 10
+    assert 10 == col.value
+
+    col = BooleanColumn()
+    col.value = "y"
+    assert col.value is True
+
+    col.value = "n"
+    assert col.value is False
+
+    col = FloatColumn()
+    col.value = " 12.33"
+    assert 12.33 == col.value
+
+    col = DecimalColumn()
+    col.value = "12.45"
+    assert Decimal("12.45") == col.value
+
+    col = DateColumn()
+    col.value = "2016-10-9"
+    assert date(2016, 10, 9) == col.value
+
+    col = DatetimeColumn()
+    col.value = "2016-10-9 11:12:3"
+    assert datetime(2016, 10, 9, 11, 12, 3) == col.value
+
+    col = TimeColumn()
+    col.value = "11:12:3"
+    assert time(11, 12, 3) == col.value
+
+
+def test_value_error():
+    with pytest.raises(ValueError, match="Can't purify 19, it's a int type."):
+        BinaryColumn().value = 19
+
+    with pytest.raises(ValueError, match="Can't purify abc, it's a str type."):
+        IntColumn().value = "abc"
+
+    with pytest.raises(ValueError, match="Can't purify abc, it's a str type."):
+        FloatColumn().value = "abc"
+
+    with pytest.raises(ValueError, match="Can't purify abc, it's a str type."):
+        DecimalColumn().value = "abc"
+
+    with pytest.raises(ValueError, match="Can't purify abc, it's a str type."):
+        DateColumn().value = "abc"
+
+    with pytest.raises(ValueError, match="Can't purify abc, it's a str type."):
+        DatetimeColumn().value = "abc"
+
+    with pytest.raises(ValueError, match="Can't purify abc, it's a str type."):
+        TimeColumn().value = "abc"
--- tests/unit/model/test_model.py
+++ tests/unit/model/test_model.py
@@ -1,15 +1,7 @@
-import unittest
-
 from sweet.sequel.terms.name_fn import Name
 from tests.helper import User
 
 
-class TestModel(unittest.TestCase):
-
-    def test_table_name(self):
-        self.assertEqual("users", User.table.name)
-        self.assertEqual(Name("users"), User.table.name_named)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_table_name():
+    assert "users" == User.table.name
+    assert Name("users") == User.table.name_named
