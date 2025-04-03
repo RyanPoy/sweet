e83e0af40e197c48c321d2d17f08edce9e3ebfe2
pengyi <ryanpoy@gmail.com>
2025-04-03 18:34:44 +0800

    replace unittest with pytest

--- tests/unit/sequel/test_raw.py
+++ tests/unit/sequel/test_raw.py
@@ -1,130 +1,110 @@
-import unittest
+import pytest
 from datetime import datetime
 from decimal import Decimal
 
 from sweet.sequel.terms.name_fn import Sum, Name
 from sweet.sequel.types import Array, Raw
-from sweet.sequel.visitors.mysql_visitor import MySQLVisitor
-from sweet.sequel.visitors.postgresql_visitor import PostgreSQLVisitor
-from sweet.sequel.visitors.sqlite_visitor import SQLiteVisitor
 
 
-class TestRaw(unittest.TestCase):
-    def setUp(self):
-        self.mysql = MySQLVisitor()
-        self.sqlite = SQLiteVisitor()
-        self.pg = PostgreSQLVisitor()
-
-    def test_init_error(self):
-        customerObj = object()
-        with self.assertRaises(TypeError) as ctx:
-            Raw(customerObj)
-        self.assertEqual(
-            "Raw initialize accept None | bool | str | bytes | int | float | decimal.Decimal | datetime.date | datetime.datetime, but got 'object'",
-            str(ctx.exception))
-
-    def test_quote(self):
-        self.assertEqual("NULL", Raw(None).quote())
-        self.assertEqual("1", Raw(True).quote())
-        self.assertEqual("0", Raw(False).quote())
-        self.assertEqual("'jim'", Raw("jim").quote())
-        self.assertEqual("'abcef'", Raw(b"abcef").quote())
-        self.assertEqual("1", Raw(1).quote())
-        self.assertEqual("1.5", Raw(1.5).quote())
-        self.assertEqual("12.75", Raw(Decimal(12.75)).quote())
-
-        s = '2025-02-13'
-        day = datetime.strptime(s, "%Y-%m-%d").date()
-        self.assertEqual("'2025-02-13'", Raw(day).quote())
-
-        s = '2025-02-13 10:03:20'
-        dt = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-        self.assertEqual("'2025-02-13 10:03:20'", Raw(dt).quote())
-
-    def test_sql(self):
-        self.assertEqual("NULL", self.mysql.sql(Raw(None)))
-        self.assertEqual("NULL", self.sqlite.sql(Raw(None)))
-        self.assertEqual("NULL", self.pg.sql(Raw(None)))
-
-        self.assertEqual("1", self.mysql.sql(Raw(True)))
-        self.assertEqual("1", self.sqlite.sql(Raw(True)))
-        self.assertEqual("1", self.pg.sql(Raw(True)))
-
-        self.assertEqual("0", self.mysql.sql(Raw(False)))
-        self.assertEqual("0", self.sqlite.sql(Raw(False)))
-        self.assertEqual("0", self.pg.sql(Raw(False)))
-
-        self.assertEqual("'jim'", self.mysql.sql(Raw("jim")))
-        self.assertEqual("'jim'", self.sqlite.sql(Raw("jim")))
-        self.assertEqual("'jim'", self.pg.sql(Raw("jim")))
-
-        self.assertEqual("'abcef'", self.mysql.sql(Raw(b"abcef")))
-        self.assertEqual("'abcef'", self.sqlite.sql(Raw(b"abcef")))
-        self.assertEqual("'abcef'", self.pg.sql(Raw(b"abcef")))
-
-        self.assertEqual("1", self.mysql.sql(Raw(1)))
-        self.assertEqual("1", self.sqlite.sql(Raw(1)))
-        self.assertEqual("1", self.pg.sql(Raw(1)))
-
-        self.assertEqual("1.5", self.mysql.sql(Raw(1.5)))
-        self.assertEqual("1.5", self.sqlite.sql(Raw(1.5)))
-        self.assertEqual("1.5", self.pg.sql(Raw(1.5)))
-
-        self.assertEqual("12.75", self.mysql.sql(Raw(Decimal(12.75))))
-        self.assertEqual("12.75", self.sqlite.sql(Raw(Decimal(12.75))))
-        self.assertEqual("12.75", self.pg.sql(Raw(Decimal(12.75))))
-
-        s = '2025-02-13'
-        day = datetime.strptime(s, "%Y-%m-%d").date()
-        self.assertEqual("'2025-02-13'", self.mysql.sql(Raw(day)))
-        self.assertEqual("'2025-02-13'", self.sqlite.sql(Raw(day)))
-        self.assertEqual("'2025-02-13'", self.pg.sql(Raw(day)))
-
-        s = '2025-02-13 10:03:20'
-        dt = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-        self.assertEqual("'2025-02-13 10:03:20'", self.mysql.sql(Raw(dt)))
-        self.assertEqual("'2025-02-13 10:03:20'", self.sqlite.sql(Raw(dt)))
-        self.assertEqual("'2025-02-13 10:03:20'", self.pg.sql(Raw(dt)))
+def test__basic_raw_init_error(visitors):
+    with pytest.raises(TypeError,
+                       match="Raw initialize accept None | bool | str | bytes | int | float | decimal.Decimal | datetime.date | datetime.datetime, but got 'object'"):
+        Raw(object())
 
 
-class TestArray(unittest.TestCase):
-    def setUp(self):
-        self.mysql = MySQLVisitor()
-        self.sqlite = SQLiteVisitor()
-        self.pg = PostgreSQLVisitor()
+def test__basic_raw_quote():
+    assert Raw(None).quote() == "NULL"
+    assert Raw(True).quote() == "1"
+    assert Raw(False).quote() == "0"
+    assert Raw("jim").quote() == "'jim'"
+    assert Raw(b"abcef").quote() == "'abcef'"
+    assert Raw(1).quote() == "1"
+    assert Raw(1.5).quote() == "1.5"
+    assert Raw(Decimal(12.75)).quote() == "12.75"
 
-    def test_init(self):
-        fn = Sum("age")
-        name = Name("username")
-        s = '2025-02-13 10:03:20'
-        now = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-        array = Array([
+    s = '2025-02-13'
+    day = datetime.strptime(s, "%Y-%m-%d").date()
+    assert Raw(day).quote() == "'2025-02-13'"
+
+    s = '2025-02-13 10:03:20'
+    dt = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    assert Raw(dt).quote() == "'2025-02-13 10:03:20'"
+
+
+def test__basic_raw_sql(visitors):
+    assert visitors.mysql.sql(Raw(None)) == "NULL"
+    assert visitors.sqlite.sql(Raw(None)) == "NULL"
+    assert visitors.pg.sql(Raw(None)) == "NULL"
+
+    assert visitors.mysql.sql(Raw(True)) == "1"
+    assert visitors.sqlite.sql(Raw(True)) == "1"
+    assert visitors.pg.sql(Raw(True)) == "1"
+
+    assert visitors.mysql.sql(Raw(False)) == "0"
+    assert visitors.sqlite.sql(Raw(False)) == "0"
+    assert visitors.pg.sql(Raw(False)) == "0"
+
+    assert visitors.mysql.sql(Raw("jim")) == "'jim'"
+    assert visitors.sqlite.sql(Raw("jim")) == "'jim'"
+    assert visitors.pg.sql(Raw("jim")) == "'jim'"
+
+    assert visitors.mysql.sql(Raw(b"abcef")) == "'abcef'"
+    assert visitors.sqlite.sql(Raw(b"abcef")) == "'abcef'"
+    assert visitors.pg.sql(Raw(b"abcef")) == "'abcef'"
+
+    assert visitors.mysql.sql(Raw(1)) == "1"
+    assert visitors.sqlite.sql(Raw(1)) == "1"
+    assert visitors.pg.sql(Raw(1)) == "1"
+
+    assert visitors.mysql.sql(Raw(1.5)) == "1.5"
+    assert visitors.sqlite.sql(Raw(1.5)) == "1.5"
+    assert visitors.pg.sql(Raw(1.5)) == "1.5"
+
+    assert visitors.mysql.sql(Raw(Decimal(12.75))) == "12.75"
+    assert visitors.sqlite.sql(Raw(Decimal(12.75))) == "12.75"
+    assert visitors.pg.sql(Raw(Decimal(12.75))) == "12.75"
+
+    s = '2025-02-13'
+    day = datetime.strptime(s, "%Y-%m-%d").date()
+    assert visitors.mysql.sql(Raw(day)) == "'2025-02-13'"
+    assert visitors.sqlite.sql(Raw(day)) == "'2025-02-13'"
+    assert visitors.pg.sql(Raw(day)) == "'2025-02-13'"
+
+    s = '2025-02-13 10:03:20'
+    dt = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    assert visitors.mysql.sql(Raw(dt)) == "'2025-02-13 10:03:20'"
+    assert visitors.sqlite.sql(Raw(dt)) == "'2025-02-13 10:03:20'"
+    assert visitors.pg.sql(Raw(dt)) == "'2025-02-13 10:03:20'"
+
+
+def test__array_raw_init():
+    fn = Sum("age")
+    name = Name("username")
+    s = '2025-02-13 10:03:20'
+    now = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    array = Array([
+        1, "1", now, fn, name, [
             1, "1", now, fn, name, [
-                1, "1", now, fn, name, [
-                    1, "1", now, fn, name]
-            ]
-        ])
-        self.assertEqual([
+                1, "1", now, fn, name]
+        ]
+    ])
+    assert array.data == [
+        Raw(1), Raw("1"), Raw(now), fn, name, Array([
             Raw(1), Raw("1"), Raw(now), fn, name, Array([
-                Raw(1), Raw("1"), Raw(now), fn, name, Array([
-                    Raw(1), Raw("1"), Raw(now), fn, name])
-            ])
-        ], array.data
-        )
-
-    def test_sql(self):
-        fn = Sum("age")
-        name = Name("username")
-        s = '2025-02-13 10:03:20'
-        now = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-        array = Array([1, "1", now, fn, name, [1, "1", now, fn, name, [1, "1", now, fn, name]]])
-        self.assertEqual("(1, '1', '2025-02-13 10:03:20', SUM(`age`), `username`, (1, '1', '2025-02-13 10:03:20', SUM(`age`), `username`, (1, '1', "
-                         "'2025-02-13 10:03:20', SUM(`age`), `username`)))", self.mysql.sql(array))
-        self.assertEqual("(1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', "
-                         "'2025-02-13 10:03:20', SUM(\"age\"), \"username\")))", self.sqlite.sql(array))
-        self.assertEqual("(1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', "
-                         "'2025-02-13 10:03:20', SUM(\"age\"), \"username\")))", self.pg.sql(array))
+                Raw(1), Raw("1"), Raw(now), fn, name])
+        ])
+    ]
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test__array_raw_sql(visitors):
+    fn = Sum("age")
+    name = Name("username")
+    s = '2025-02-13 10:03:20'
+    now = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    array = Array([1, "1", now, fn, name, [1, "1", now, fn, name, [1, "1", now, fn, name]]])
+    assert visitors.mysql.sql(array) == \
+           "(1, '1', '2025-02-13 10:03:20', SUM(`age`), `username`, (1, '1', '2025-02-13 10:03:20', SUM(`age`), `username`, (1, '1', '2025-02-13 10:03:20', SUM(`age`), `username`)))"
+    assert visitors.sqlite.sql(array) == \
+           "(1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\")))"
+    assert visitors.pg.sql(array) == \
+           "(1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\", (1, '1', '2025-02-13 10:03:20', SUM(\"age\"), \"username\")))"
