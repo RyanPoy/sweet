f41e5a689db0699a7a9d0bfc52a2bb6e0286cb19
pengyi <ryanpoy@gmail.com>
2025-04-03 21:35:37 +0800

    refactor

--- tests/helper/sqls/mysql_sql.py
+++ tests/helper/sqls/mysql_sql.py
@@ -1,5 +1,12 @@
 CREATE_SQLS = [
-    # create table users
+    # create table foos for test_mysql_driver
+    """CREATE TABLE IF NOT EXISTS `foos` (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        code INT NOT NULL
+    )""",
+
+    # create table users for the User model class
     """CREATE TABLE IF NOT EXISTS users (
         id int auto_increment primary key,
         name varchar(64) not null
@@ -8,5 +15,6 @@ CREATE_SQLS = [
 ]
 
 DROP_SQLS = [
+    """DROP TABLE IF EXISTS foos""",
     """DROP TABLE IF EXISTS users""",
 ]
