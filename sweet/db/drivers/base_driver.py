762c4a8f7ad93590e5f29827608203eaed4befc9
pengyi <ryanpoy@gmail.com>
2025-04-14 18:32:04 +0800

    fix bug

--- sweet/db/drivers/base_driver.py
+++ sweet/db/drivers/base_driver.py
@@ -1,128 +1,98 @@
-from __future__ import annotations
-
 from abc import ABC, abstractmethod
 from contextlib import asynccontextmanager
-from typing import Any, AsyncIterator
+from contextvars import ContextVar
+from sweet.utils.logger import get_logger
 
-from sweet.db.transaction import Transaction
+logger = get_logger()
 
 
 class BaseDriver(ABC):
+    def __init__(self, **db_config):
+        self._local_connection = ContextVar('connection')  # 协程局部变量，用于存储连接
+
     @abstractmethod
+    async def init_pool(self, minsize=1, maxsize=10):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def close_pool(self):
+        """ close the connection pool """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_connection(self):
+        """ get the connection of current coroutine """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def set_autocommit(self, conn, auto=True):
+        raise NotImplementedError
+
     @asynccontextmanager
-    async def transaction(self) -> AsyncIterator[Transaction]: pass
+    async def transaction(self):
+        conn = None
+        try:
+            conn = await self.get_connection()
+            await self.set_autocommit(conn, False)
+            yield self
+            await conn.commit()
+        except Exception:
+            if conn: await conn.rollback()
+            raise
+        finally:
+            if conn: await self.set_autocommit(conn)
+
+    async def fetchone(self, sql, *params):
+        """Returns the first row returned for the given query."""
+        async with self._execute(sql, *params) as cursor:
+            row = await cursor.fetchone()
+            if row is None:
+                return None
+            columns = [col[0] for col in cursor.description]
+            return dict(zip(columns, row))
+
+    async def fetchall(self, sql, *params):
+        """Returns a row list for the given query and parameters."""
+        async with self._execute(sql, *params) as cursor:
+            columns = [col[0] for col in cursor.description]
+            rows = await cursor.fetchall()
+            return [dict(zip(columns, row)) for row in rows]
+
+    async def execute_lastrowid(self, sql, *params):
+        """Executes the given query, returning the lastrowid from the query."""
+        async with self._execute(sql, *params) as cursor:
+            return cursor.lastrowid
+
+    async def execute_returnrowid(self, sql, *params):
+        """Executes the given query, returning the lastrowid from the query."""
+        async with self._execute(sql, *params) as cursor:
+            return cursor.lastrowid
+
+    async def execute_rowcount(self, sql, *params):
+        """Executes the given query, returning the rowcount from the query."""
+        async with self._execute(sql, *params) as cursor:
+            return cursor.rowcount
+
+    async def execute(self, sql, *params):
+        """Executes the given query, returning the rowcount from the query."""
+        async with self._execute(sql, *params):
+            return self
+
+    @asynccontextmanager
+    async def _execute(self, sql, *params):
+        logger.debug(sql)
+        connection = await self.get_connection()
+        cursor = None
+        try:
+            cursor = await connection.cursor()
+            await cursor.execute(sql, params)
+            yield cursor
+        finally:
+            if cursor:
+                r = cursor.close()
+                if r is not None:
+                    await r
 
     @abstractmethod
-    async def execute_rowid(self, sql: str, *params: Any) -> Any: pass
-
-    @abstractmethod
-    async def execute_rowids(self, sql: str, params_seq: list[tuple]) -> list: pass
-
-    @abstractmethod
-    async def execute_rowcount(self, sql: str, *params: Any) -> int: pass
-
-    @abstractmethod
-    async def fetchone(self, sql: str, *params: Any) -> dict[str, Any] | None: pass
-
-    @abstractmethod
-    async def fetchall(self, sql: str, *params: Any) -> list[dict[str, Any]] | None: pass
-
-
-# from abc import ABC, abstractmethod
-# from contextlib import asynccontextmanager
-# from contextvars import ContextVar
-# from sweet.utils.logger import get_logger
-#
-# logger = get_logger()
-#
-#
-# class BaseDriver(ABC):
-#     def __init__(self, **db_config):
-#         self._local_connection = ContextVar('connection')  # 协程局部变量，用于存储连接
-#
-#     @abstractmethod
-#     async def init_pool(self, minsize=1, maxsize=10):
-#         raise NotImplementedError
-#
-#     @abstractmethod
-#     async def close_pool(self):
-#         """ close the connection pool """
-#         raise NotImplementedError
-#
-#     @abstractmethod
-#     async def get_connection(self):
-#         """ get the connection of current coroutine """
-#         raise NotImplementedError
-#
-#     @abstractmethod
-#     async def set_autocommit(self, conn, auto=True):
-#         raise NotImplementedError
-#
-#     @asynccontextmanager
-#     async def transaction(self):
-#         conn = None
-#         try:
-#             conn = await self.get_connection()
-#             await self.set_autocommit(conn, False)
-#             yield self
-#             await conn.commit()
-#         except Exception:
-#             if conn: await conn.rollback()
-#             raise
-#         finally:
-#             if conn: await self.set_autocommit(conn)
-#
-#     async def fetchone(self, sql, *params):
-#         """Returns the first row returned for the given query."""
-#         async with self._execute(sql, *params) as cursor:
-#             row = await cursor.fetchone()
-#             if row is None:
-#                 return None
-#             columns = [col[0] for col in cursor.description]
-#             return dict(zip(columns, row))
-#
-#     async def fetchall(self, sql, *params):
-#         """Returns a row list for the given query and parameters."""
-#         async with self._execute(sql, *params) as cursor:
-#             columns = [col[0] for col in cursor.description]
-#             rows = await cursor.fetchall()
-#             return [dict(zip(columns, row)) for row in rows]
-#
-#     async def execute_lastrowid(self, sql, *params):
-#         """Executes the given query, returning the lastrowid from the query."""
-#         async with self._execute(sql, *params) as cursor:
-#             return cursor.lastrowid
-#
-#     async def execute_returnrowid(self, sql, *params):
-#         """Executes the given query, returning the lastrowid from the query."""
-#         async with self._execute(sql, *params) as cursor:
-#             return cursor.lastrowid
-#
-#     async def execute_rowcount(self, sql, *params):
-#         """Executes the given query, returning the rowcount from the query."""
-#         async with self._execute(sql, *params) as cursor:
-#             return cursor.rowcount
-#
-#     async def execute(self, sql, *params):
-#         """Executes the given query, returning the rowcount from the query."""
-#         async with self._execute(sql, *params):
-#             return self
-#
-#     @asynccontextmanager
-#     async def _execute(self, sql, *params):
-#         logger.debug(sql)
-#         connection = await self.get_connection()
-#         cursor = None
-#         try:
-#             cursor = await connection.cursor()
-#             await cursor.execute(sql, params)
-#             yield cursor
-#         finally:
-#             if cursor:
-#                 r = cursor.close()
-#                 if r is not None:
-#                     await r
-#
-#     @abstractmethod
-#     async def columns(self, table_name: str) -> list[dict]:
-#         raise NotImplementedError
+    async def columns(self, table_name: str) -> list[dict]:
+        raise NotImplementedError
--- sweet/db/drivers/sqlite_driver.py
+++ sweet/db/drivers/sqlite_driver.py
@@ -1,134 +1,138 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Self
 
 import aiosqlite
 from contextlib import asynccontextmanager
 
+from sweet.db import SQLitePool
 from sweet.db.drivers.base_driver import BaseDriver
-from sweet.db.transaction import BaseTransaction
+from sweet.db.transaction import Transaction
+
+#
+# class SQLiteDriver(BaseDriver):
+#     def __init__(self, **db_config):
+#         self.pool = SQLitePool(**db_config)
+#
+#     async def initialize(self, **db_config) -> Self:
+#         await self.pool.initialize()
+#         return self
+#
+#     @asynccontextmanager
+#     async def transaction(self):
+#         async with aiosqlite.connect(self.db_path) as conn:
+#             await conn.execute('BEGIN')
+#             try:
+#                 yield Transaction(conn)
+#                 await conn.commit()
+#             except Exception:
+#                 await conn.rollback()
+#                 raise
+#
+#     async def execute_rowid(self, sql: str, *params):
+#         async with aiosqlite.connect(self.db_path) as conn:
+#             async with conn.cursor() as cur:
+#                 await cur.execute(sql, params)
+#                 return cur.lastrowid
+#
+#     async def execute_rowids(self, sql: str, params_seq):
+#         ids = []
+#         async with aiosqlite.connect(self.db_path) as conn:
+#             async with conn.cursor() as cur:
+#                 for params in params_seq:
+#                     await cur.execute(sql, params)
+#                     ids.append(cur.lastrowid)
+#         return ids
+#
+#     async def execute_rowcount(self, sql: str, *params):
+#         async with aiosqlite.connect(self.db_path) as conn:
+#             async with conn.cursor() as cur:
+#                 await cur.execute(sql, params)
+#                 return cur.rowcount
+#
+#     async def fetchone(self, sql: str, *params: Any) -> dict[str, Any] | None:
+#         async with aiosqlite.connect(self.db_path) as conn:
+#             conn.row_factory = aiosqlite.Row
+#             async with conn.execute(sql, params) as cur:
+#                 row = await cur.fetchone()
+#                 return dict(row) if row else None
+#
+#     async def fetchall(self, sql: str, *params: Any) -> list[dict[str, Any]] | None:
+#         async with aiosqlite.connect(self.db_path) as conn:
+#             conn.row_factory = aiosqlite.Row
+#             async with conn.execute(sql, params) as cur:
+#                 rows = await cur.fetchall()
+#                 return [dict(r) for r in rows] if rows else None
+
+from asyncio import Queue
+
+from sweet.db.drivers.base_driver import BaseDriver
+import aiosqlite
 
 
 class SQLiteDriver(BaseDriver):
-    def __init__(self, db_path: str):
-        self.db_path = db_path
 
-    @asynccontextmanager
-    async def transaction(self):
-        async with aiosqlite.connect(self.db_path) as conn:
-            await conn.execute('BEGIN')
-            try:
-                yield BaseTransaction(conn)
-                await conn.commit()
-            except Exception:
-                await conn.rollback()
-                raise
+    def __init__(self, **db_config):
+        """
+        kwargs contain:
+            db,
+            charset='utf8',
+            show_sql=False
+        """
+        super().__init__()
+        self.db_config = db_config
+        self.db_config['check_same_thread'] = False
+        self.pool = None
 
-    async def execute_rowid(self, sql: str, *params):
-        async with aiosqlite.connect(self.db_path) as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(sql, params)
-                return cur.lastrowid
+    async def init_pool(self, minsize=1, maxsize=10):
+        """ initialize connection pool
+        """
+        self.pool = Queue(maxsize + 2)
+        for x in range(maxsize):
+            conn = await aiosqlite.connect(self.db_config['db'], check_same_thread=self.db_config['check_same_thread'])
+            await self.pool.put(conn)
+        return self
 
-    async def execute_rowids(self, sql: str, params_seq):
-        ids = []
-        async with aiosqlite.connect(self.db_path) as conn:
-            async with conn.cursor() as cur:
-                for params in params_seq:
-                    await cur.execute(sql, params)
-                    ids.append(cur.lastrowid)
-        return ids
+    async def close_pool(self):
+        """ close the connection pool """
+        await self._release_connection()
+        if self.pool:
+            while not self.pool.empty():
+                conn = await self.pool.get()
+                await conn.close()
 
-    async def execute_rowcount(self, sql: str, *params):
-        async with aiosqlite.connect(self.db_path) as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(sql, params)
-                return cur.rowcount
+    async def _release_connection(self):
+        """ release the connection of current coroutine """
+        connection = self._local_connection.get(None)
+        if connection:
+            self._local_connection.set(None)
+            await self.pool.put(connection)
 
-    async def fetchone(self, sql: str, *params: Any) -> dict[str, Any] | None:
-        async with aiosqlite.connect(self.db_path) as conn:
-            conn.row_factory = aiosqlite.Row
-            async with conn.execute(sql, params) as cur:
-                row = await cur.fetchone()
-                return dict(row) if row else None
+    async def get_connection(self):
+        """ get the connection of current coroutine """
+        connection = self._local_connection.get(None)
+        if connection is None:
+            connection = await self.pool.get()
+            await self.set_autocommit(connection)
+            self._local_connection.set(connection)
+        return connection
 
-    async def fetchall(self, sql: str, *params: Any) -> list[dict[str, Any]] | None:
-        async with aiosqlite.connect(self.db_path) as conn:
-            conn.row_factory = aiosqlite.Row
-            async with conn.execute(sql, params) as cur:
-                rows = await cur.fetchall()
-                return [dict(r) for r in rows] if rows else None
+    async def set_autocommit(self, conn, auto=True):
+        if auto is True:
+            conn.isolation_level = None
+        else:
+            conn.isolation_level = 'DEFERRED'
+        return self
 
+    async def columns(self, table_name: str) -> list[dict]:
+        sql = f"PRAGMA table_info({table_name})"
+        rows = await self.fetchall(sql)
+        return [
+            {'name': r['name'], 'kind': r['type'], 'null': r['notnull'] == 0, 'key': '', 'default': r['dflt_value'], 'extra': str(r['pk'])} for r in rows
+        ]
 
-# from asyncio import Queue
-#
-# from sweet.db.drivers.base_driver import BaseDriver
-# import aiosqlite
-#
-#
-# class SQLiteDriver(BaseDriver):
-#
-#     def __init__(self, **db_config):
-#         """
-#         kwargs contain:
-#             db,
-#             charset='utf8',
-#             show_sql=False
-#         """
-#         super().__init__()
-#         self.db_config = db_config
-#         self.db_config['check_same_thread'] = False
-#         self.pool = None
-#
-#     async def init_pool(self, minsize=1, maxsize=10):
-#         """ initialize connection pool
-#         """
-#         self.pool = Queue(maxsize + 2)
-#         for x in range(maxsize):
-#             conn = await aiosqlite.connect(self.db_config['db'], check_same_thread=self.db_config['check_same_thread'])
-#             await self.pool.put(conn)
-#         return self
-#
-#     async def close_pool(self):
-#         """ close the connection pool """
-#         await self._release_connection()
-#         if self.pool:
-#             while not self.pool.empty():
-#                 conn = await self.pool.get()
-#                 await conn.close()
-#
-#     async def _release_connection(self):
-#         """ release the connection of current coroutine """
-#         connection = self._local_connection.get(None)
-#         if connection:
-#             self._local_connection.set(None)
-#             await self.pool.put(connection)
-#
-#     async def get_connection(self):
-#         """ get the connection of current coroutine """
-#         connection = self._local_connection.get(None)
-#         if connection is None:
-#             connection = await self.pool.get()
-#             await self.set_autocommit(connection)
-#             self._local_connection.set(connection)
-#         return connection
-#
-#     async def set_autocommit(self, conn, auto=True):
-#         if auto is True:
-#             conn.isolation_level = None
-#         else:
-#             conn.isolation_level = 'DEFERRED'
-#         return self
-#
-#     async def columns(self, table_name: str) -> list[dict]:
-#         sql = f"PRAGMA table_info({table_name})"
-#         rows = await self.fetchall(sql)
-#         return [
-#             {'name': r['name'], 'kind': r['type'], 'null': r['notnull'] == 0, 'key': '', 'default': r['dflt_value'], 'extra': str(r['pk'])} for r in rows
-#         ]
-#
-#     async def execute_returnrowid(self, sql, *params):
-#         """Executes the given query, returning the lastrowid from the query."""
-#         async with self._execute(sql, *params) as cursor:
-#             row = await cursor.fetchone()
-#             return row[0]  # 打印返回的 id
+    async def execute_returnrowid(self, sql, *params):
+        """Executes the given query, returning the lastrowid from the query."""
+        async with self._execute(sql, *params) as cursor:
+            row = await cursor.fetchone()
+            return row[0]  # 打印返回的 id
